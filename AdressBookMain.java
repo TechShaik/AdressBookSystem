d8bd478a8aecd490d30f70b7cafec9f001b8cbd0
shaiksha <shaiknaveed5811@gmail.com>
2024-08-28 14:50:29 +0530

    Contact details added

--- AdressBookMain.java
+++ AdressBookMain.java
@@ -4,6 +4,21 @@ public class AdressBookMain {
 
 	public static void main(String[] args) {
       System.out.println("Welcome to Adress Program");
+      
+      
+      // UC-2
+      
+      Contacts person1=new Contacts();
+      person1.First_name="Tailor";
+      person1.Last_name="Shaikshavali";
+      person1.Adress="near BTM water tank,BTM Layout";
+      person1.City="Banglore";
+      person1.State="Karnataka";
+      person1.zip=560029;
+      person1.Phone_number=6303833290L;
+      person1.E_mail="shaiknaveed5811@gmail.com";
+      
+       
 	}
 
 }
