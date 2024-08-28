5e1483ef8b3469b0034be20126cdb7d4e06d67a6
shaiksha <shaiknaveed5811@gmail.com>
2024-08-28 14:33:27 +0530

    Contacts file added

---
+++ Contacts.java
@@ -0,0 +1,17 @@
+package com.files;
+
+public class Contacts {
+
+	 
+                String First_name;
+                String Last_name;
+                String Adress;
+                String City;
+                String State;
+                int zip;
+                long Phone_number;
+                String E_mail;
+                
+	
+
+}
