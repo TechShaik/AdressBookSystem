b63357c3a112a480d9a066bf7e38c1870c4b99dd
shaiksha <shaiknaveed5811@gmail.com>
2024-08-29 12:16:22 +0530

    updated upto adding muliple users at once

--- AdressBookMain.java
+++ AdressBookMain.java
@@ -4,71 +4,104 @@ import java.util.Scanner;
 
 public class AdressBookMain {
 
-	public static void main(String[] args) {
-         Scanner sc=new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Adding ad = new Adding();
 
-      System.out.println("Welcome to Adress Program");
-      System.out.println();
-    
-         
-        /// add here
-         Adding ad=new Adding();
-         
-         Contacts p1=new Contacts("Tailor","Shaikshavali","BTM Layout","Banglore","Karnataka",560029,6303833290L,"shaiknaveed5811@gmail.com");
-         Contacts p2=new Contacts("Ternekal","Shahid","Udipi","hyd","ap",568889,6281882610L,"sayyedshahid@gmail.com");
+        System.out.println("Welcome to Address Program");
+        System.out.println();
 
- 
-         ad.add(p1);
-         ad.add(p2);       
-         
+        while (true) {
+             System.out.println("Select your operation:");
+            System.out.println("Press 1 to add new contact");
+                System.out.println("Press 2 to edit contact");
+            System.out.println("Press 3 to delete contact");
+                 System.out.println("Press 4 to view contacts");
+            System.out.println("Press 5 to exit");
 
-         System.out.println("Select your operation:");
-         System.out.println("Press 1 to edit contact");
-         System.out.println("Press 2 to delete contact");
-         System.out.println("Press 3 to view contacts");
-         System.out.println("Press 4 to exit");
+            int n = sc.nextInt();
+            sc.nextLine(); 
 
-        int n=sc.nextInt();
-        sc.nextLine();
-        switch(n) {
-       
-        case 1:
-        	
-        	 System.out.println("Enter the first name");
-	            String F_name=sc.nextLine();
-	            
-	            System.out.println("Enter the last name");
-	            String l_name=sc.nextLine();
-	            
-	            ad.edit(F_name,l_name);  
-	            
-                ad.display();
-      	            break; 
-      	            
-        case 2:
-        	
+            switch (n) {
+                case 1:
+                    
+                    while (true) {
+                        System.out.println("Enter details for a new contact:");
+                           System.out.println("Enter first name:");
+                        String firstName = sc.nextLine();
 
-       	 System.out.println("Enter the first name");
-	            String F_nam=sc.nextLine();
-	            
-	            System.out.println("Enter the last name");
-	            String l_nam=sc.nextLine();
-	            ad.delete(F_nam, l_nam);
-	            
-                ad.display();	            
-  	            break;
-  	            
-        case 3: 
-        	
-                      ad.display();
-                         break;
-                         
-        case 4:
-        	
-        	System.out.println("Invalid Option");
-            sc.close();
-            break;
-        	     }
-      }
-	
+                          System.out.println("Enter last name:");
+                        String lastName = sc.nextLine();
+
+                          System.out.println("Enter address:");
+                        String address = sc.nextLine();
+
+                            System.out.println("Enter city:");
+                        String city = sc.nextLine();
+
+                        System.out.println("Enter state:");
+                           String state = sc.nextLine();
+
+                        System.out.println("Enter zip code:");
+                          int zip = sc.nextInt();
+                         sc.nextLine(); 
+
+                            System.out.println("Enter phone number:");
+                          long phoneNumber = sc.nextLong();
+                             sc.nextLine(); 
+                        System.out.println("Enter email:");
+                          String email = sc.nextLine();
+
+                           Contacts contact = new Contacts(firstName, lastName, address, city, state, zip, phoneNumber, email);
+                        ad.add(contact);
+
+                        System.out.println("Contact added successfully!");
+
+                          System.out.println("Do you want to add another contact? (yes/no)");
+                        String res = sc.nextLine();
+                        if (!res.equalsIgnoreCase("yes")) {
+                             break; 
+                        }
+                    }
+                    break;
+
+                case 2:
+                     System.out.println("Enter the first name");
+                    String F_name = sc.nextLine();
+
+                    System.out.println("Enter the last name");
+                    String l_name = sc.nextLine();
+
+                    ad.edit(F_name, l_name);
+                    ad.display();
+                    break;
+
+                case 3:
+                     System.out.println("Enter the first name");
+                    String F_nam = sc.nextLine();
+
+                    System.out.println("Enter the last name");
+                    String l_nam = sc.nextLine();
+
+                    ad.delete(F_nam, l_nam);
+                    ad.display();
+                    break;
+
+                case 4:
+                     ad.display();
+                    break;
+
+                case 5:
+                     System.out.println("Exiting the program.");
+                    sc.close();
+                    return;  
+
+                default:
+                    System.out.println("Invalid Option Exiting the program.");
+                    sc.close();
+
+                    break;
+            }
+        }
+    }
 }
